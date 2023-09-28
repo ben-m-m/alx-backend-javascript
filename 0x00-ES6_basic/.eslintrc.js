539073519a7e64a24f18600d5fdb48e52c3e9c5f
ben-m-m <mainaben23@gmail.com>
2023-09-28 10:40:43 +0300

    changes

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,36 +1,34 @@
-
 module.exports = {
   env: {
-    browser: false,
+    browser: true,
     es6: true,
     jest: true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:jest/all',
-  ],
+  extends: ["airbnb-base", "plugin:jest/all"],
+  plugins: ["jest"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+	  excludedFiles: "babel.config.js",
+    },
+  ],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['jest'],
   rules: {
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
+    "no-console": "off",
+    "no-shadow": "off",
+    "no-restricted-syntax": ["error", "LabelStatement", "WithStatement"],
   },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
 };

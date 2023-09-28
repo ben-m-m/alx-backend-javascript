c93c807ae4bac0dde49b687e13983b47e98bed56
ben-m-m <mainaben23@gmail.com>
2023-09-28 19:56:37 +0300

    hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -15,8 +15,6 @@ export class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-
-
 export class StudentHolberton extends HolbertonClass {
   constructor(firstName, lastName, holbertonClass) {
     super(holbertonClass.year, holbertonClass.location);
@@ -43,6 +41,5 @@ const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
 
 export default [student1, student2, student3, student4, student5];

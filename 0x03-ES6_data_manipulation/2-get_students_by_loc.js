669bd3da47097e526ce8ad5c289322e9b5e13fb8
ben-m-m <mainaben23@gmail.com>
2023-10-03 11:43:11 +0300

    get student by location

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,6 @@
 export default function getStudentsByLocation(students, city) {
+  if (Array.isArray(students) !== true) {
+    return [];
+  }
   return students.filter((student) => student.location === city);
 }

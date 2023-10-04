da25f2cac61be5c9663cfa3343d590a40da07471
ben-m-m <mainaben23@gmail.com>
2023-10-04 14:02:18 +0300

    lint compliant

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,4 @@
-export default function hasValuesFromArray (set, array) {
+export default function hasValuesFromArray(set, array) {
   for (const element of array) {
     if (!set.has(element)) {
       return false;

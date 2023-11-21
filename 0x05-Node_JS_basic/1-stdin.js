5bdfc7495c05b9eb0ea307147aa220add9525bd1
ben-m-m <mainaben23@gmail.com>
2023-11-21 12:31:07 +0300

    using process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  if (name.length > 0) {
+    console.log(`Your name is: ${name}`);
+  } else {
+    console.log('Your name is: ');
+  }
+});
+
+process.on('SIGINT', () => {
+  console.log('\nThis important software is now closing');
+  process.exit(0);
+});

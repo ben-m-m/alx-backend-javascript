3a76db0f8c4800885917f7c57b88db01200bd41c
ben-m-m <mainaben23@gmail.com>
2023-11-23 10:58:23 +0300

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -21,20 +21,20 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
         const studentRecord = line.split(',');
         const studentPropValues = studentRecord.slice(
           0,
-          studentRecord.length - 1,
+          studentRecord.length - 1
         );
         const field = studentRecord[studentRecord.length - 1];
         studentGroups[field] = studentGroups[field] || [];
         const studentEntries = studentPropNames.map((propName, idx) => [
           propName,
-          studentPropValues[idx],
+          studentPropValues[idx]
         ]);
         studentGroups[field].push(Object.fromEntries(studentEntries));
       }
 
       const totalStudents = Object.values(studentGroups).reduce(
         (pre, cur) => (pre || []).length + cur.length,
-        0,
+        0
       );
       console.log(`Number of students: ${totalStudents}`);
       for (const [field, group] of Object.entries(studentGroups)) {
@@ -42,7 +42,7 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
           .map((student) => student.firstname)
           .join(', ');
         console.log(
-          `Number of students in ${field}: ${group.length}. List: ${studentNames}`,
+          `Number of students in ${field}: ${group.length}. List: ${studentNames}`
         );
       }
       resolve(true);

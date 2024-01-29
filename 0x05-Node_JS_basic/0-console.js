88e5f621ed02a7b2d4dd25f2fb4510ca190b9ed7
ben-m-m <mainaben23@gmail.com>
2024-01-29 12:25:55 +0300

    print str to STDOUT

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 /*
  eslint-disable
-*/
+ */
+ /*function displayMessage prints in STDOUT the message
+ */
 
 function displayMessage(message) {
-    console.log(message);
+  console.log(message);
 }
 
 module.exports = displayMessage;

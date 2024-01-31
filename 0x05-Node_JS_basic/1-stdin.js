3cbef9ab96f9f151e27d325acddee2bd801888f2
ben-m-m <mainaben23@gmail.com>
2024-01-31 08:33:04 +0300

    nulling coalescing in ESlint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 /* eslint-disable jest/require-hook */
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
-  const input = process.stdin.read() || '';
+  const input = process.stdin.read();
   if (input !== null) {
     process.stdout.write(`Your name is: ${input}`);
   }
